c966f70b8dfdcb036e1f2dc143a6f42b1937ab59
Sandeep0151 <sandeepyaduvanshi28@gmail.com>
2023-04-09 02:22:01 +0530

    Initial Commit

--- Nathpublication/asgi.py
+++ Nathpublication/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Nathpublication project.
+ASGI config for Nathpublication Nathpublication.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- Nathpublication/wsgi.py
+++ Nathpublication/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Nathpublication project.
+WSGI config for Nathpublication Nathpublication.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
